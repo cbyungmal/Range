e3dec142babe6b5ad7b6ab03b0462aebc520eb94
cbyungmal <jgaines554@noscihigh.org>
2021-03-08 18:15:33 +0000

    What was told.

--- index.js
+++ index.js
@@ -19,9 +19,7 @@
  */
 function range(start, end) {
     // YOUR CODE GOES BELOW HERE // 
- 
-    
-    
-    
+ var result = range (9, 18); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+    console.log(result);
     // YOUR CODE GOES ABOVE HERE //
 }
